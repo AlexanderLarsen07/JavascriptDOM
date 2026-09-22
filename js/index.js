e47b3188454c77648a6629715254fafdfe3ab2da
Alexander Larsen <all005@edu.zealand.dk>
2026-09-22 09:58:56 +0200

    task 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function BeregnSum(a, b){
+return a + b;
+}
+
+console.log(BeregnSum(12, 3));
+
+let liste = document.getElementById("list");
+
+function AddElement(textnode, attribute, id){
+    let NewElement = document.createElement("li");
+    NewElement.setAttribute("id", id);
+    NewElement.setAttribute("class", attribute);
+    NewTextNode = document.createTextNode(textnode);
+    NewElement.appendChild(NewTextNode);
+
+    return NewElement;
+}
+
+liste.appendChild(AddElement("banana", "healthy", "4"));
+console.log(liste);
+
+
