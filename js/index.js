b198a641989e103f2a9357294af54bf2ebd9eecf
Alexander Larsen <all005@edu.zealand.dk>
2026-09-22 10:10:00 +0200

    task 2

--- js/index.js
+++ js/index.js
@@ -9,11 +9,11 @@ console.log(BeregnSum(12, 3));
 
 let liste = document.getElementById("list");
 
-function AddElement(textnode, attribute, id){
+function AddElement(textNode, attribute, id){
     let NewElement = document.createElement("li");
     NewElement.setAttribute("id", id);
     NewElement.setAttribute("class", attribute);
-    NewTextNode = document.createTextNode(textnode);
+    NewTextNode = document.createTextNode(textNode);
     NewElement.appendChild(NewTextNode);
 
     return NewElement;
@@ -22,4 +22,9 @@ function AddElement(textnode, attribute, id){
 liste.appendChild(AddElement("banana", "healthy", "4"));
 console.log(liste);
 
+function AppendElement(listId, element){
+let list = document.getElementById(listId);
+list.appendChild(element);
+}
 
+AppendElement("listUnhealthy", AddElement("Crack", "unhealthy", "5"));
